49db3927672ceb5a69da342c8879658e01115ba2
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2025-05-03 00:28:34 +0800

    Add files via upload

---
+++ netlify/functions/next_server.js
@@ -0,0 +1,6 @@
+import { createRequestHandler } from "@netlify/next"
+
+export const handler = createRequestHandler({
+  // If you have custom Next.js configuration, you can provide it here
+  // customConfig: { ... }
+})
